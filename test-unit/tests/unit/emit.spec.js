becfd494c08682b05b08f837986519a610df65d1
uni51 <uni51@users.noreply.github.com>
2020-01-05 12:18:49 +0900

    10-1-7 カスタムイベントを伴うテスト

---
+++ test-unit/tests/unit/emit.spec.js
@@ -0,0 +1,19 @@
+import MyEmit from '@/components/MyEmit.vue'
+import { shallowMount } from '@vue/test-utils'
+
+
+it('$emit Test', () => {
+  const wrapper = shallowMount(MyEmit)
+  // ボタンをクリック
+  wrapper.find('input').trigger('click')
+  // カスタムイベントをクリック
+  const emit = wrapper.emitted()
+  console.log(emit) // 結果：{ update: [ [], [ [Object] ] ] }
+
+  // updateイベントが発生しているか
+  expect(emit.update).toBeTruthy()
+  // updateイベントが何回発生したか
+  expect(emit.update.length).toBe(2)
+  // 2回目のupdateイベントのデータを確認
+  expect(emit.update[1][0].version).toBe('2.6.10')
+})
