fe3c93507e5991d7e368fd0ffa0f09cabf274460
uni51 <uni51@users.noreply.github.com>
2020-01-05 11:54:02 +0900

    10-1-5 算出プロパティのテスト

---
+++ test-unit/src/components/MyCompute.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="email">{{ localEmail }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    // 文字列型のemail属性
+    email: String
+  },
+  // メールアドレスの「@」以前を取得
+  computed: {
+    localEmail: function() {
+      return this.email.split('@')[0].toLowerCase()
+    }
+  }
+}
+</script>
